8934327100eb58fab68d4bbcaccb8e084e514e31
Mike Jed <jedmike@gmail.com>
2020-03-09 19:41:20 -0400

    working on use State correctly with Dan

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -2,13 +2,13 @@ import React from "react"
 import "./style.css"
 
 function FriendCard(props) {
-  const contentClass = props.squareColor.toLowerCase() === "white" ? "cardW" : "cardB"
-  console.log("FriendCard -> contentClass", contentClass)
-  console.log(props)
+  const contentClass = props.squareColor === "Teal" ? "cardT" : "cardB"
+  // console.log("FriendCard -> contentClass", contentClass)
+  // console.log(props.id)
 
   return (
-    <div className={contentClass}id={props.id} onClick={props.handleClick} key={props.id}>
-    {props.id}
+    <div className={contentClass}id={props.id} onClick={props.setColor}  onClick={props.handleClick} key={props.id}>
+    {props.squareColor}
       
     </div>
   )
--- src/components/FriendCard2/index.js
+++ src/components/FriendCard2/index.js
@@ -2,9 +2,9 @@ import React from "react"
 import "./style.css"
 
 function FriendCard2(props) {
-  const contentClass = props.squareColor.toLowerCase() === "white" ? "cardB" : "cardW"
-  console.log("FriendCard2 -> contentClass", contentClass)
-  console.log(props)
+   const contentClass = props.squareColor === "White" ? "cardB" : "cardW"
+  // console.log("FriendCard2 -> contentClass", contentClass)
+  // console.log(props.id)
 
   return (
     <div className={contentClass}id={props.id} onClick={props.handleClick} key={props.id}>
