10f6c147e0399b3571708c918885f25272d536b9
Mike Jed <jedmike@gmail.com>
2020-03-11 09:51:02 -0400

    added base server and mongoose db model

--- models/book.js
+++ models/book.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
+const Game = new Schema({
+  Name: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
 
-const Book = mongoose.model("Book", bookSchema);
+const Game = mongoose.model("Game", GameSchema);
 
 module.exports = Book;
--- server.js
+++ server.js
@@ -1,102 +1,24 @@
-
-
-
-
-// Start the API server
-// app.listen(PORT, function() {
-//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-// });ls
-
-
-//********************************************************* */
-//variable, constants
-const mongoose = require("mongoose");
-const http = require("http");
 const express = require("express");
-const socketio = require("socket.io");
-// const BattleshipGame = require('./BattleshipGame.js');
-const app = express();
-const port = process.env.PORT || 3000;
-const clientPath = './client/';
-const server = http.createServer(app);
-const io = socketio(server);
 
-// const routes = require("./routes");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // Add routes, both API and view
-// app.use(routes);
+app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
-
-//********************************************************* */
-//Server start
-
-console.log('Serving static from ${clientPath}');
-
-app.use(express.static(clientPath));
-
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-//Minimum socket working connectionn
-io.on('connection', (socket) => {
-    new Btl(waitingPlayer, socket);
-    console.log('A player connected');
-    console.log((new Date().toISOString()) + ' ID ' + socket.id + ' connected.');
-    socket.emit('message', 'You are connected ');
-})
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-// let waitingPlayer = null;
-
-// io.on('connection', (socket) => {
-//     // console.log('beep');
-//     if (waitingPlayer) {
-//         // console.log('beep beep');
-//         new BattleshipGame(waitingPlayer, socket);
-//         waitingPlayer = null;
-//     } else {
-//         waitingPlayer = socket;
-//         waitingPlayer.emit('message', 'Waiting for an opponent');
-//     }
-
-
-//     socket.on('message', (text) => {
-//         io.emit('message', text);
-//     });
-// });
-
-
-server.on("error", (err) => {
-    console.error("Server error:", err);
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-
-server.listen(port, () => {
-    console.log("Battleship Game started on port:" + port);
-});
-
-//codeNeededcodeNeededcodeNeededcodeNeededcodeNeededcodeNeeded//
-//Server issue 1:
-//connect to Battleship.db
-//initialize it
-//codeNeededcodeNeededcodeNeededcodeNeededcodeNeededcodeNeeded//
-
-//codeNeededcodeNeededcodeNeededcodeNeededcodeNeededcodeNeeded//
-//Server issue 2:
-//listen for Player name
-//listen for Oponent name
-//add to database
-//      log moves and grids for replay, persistence
-//codeNeededcodeNeededcodeNeededcodeNeededcodeNeededcodeNeeded//
-
-
-
-// console.log(clientPath);
